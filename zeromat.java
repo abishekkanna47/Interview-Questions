5710b39711cffa01ac0d8bcea144f126c7ad4c17
AK <93335850+abishekkanna47@users.noreply.github.com>
2023-06-20 21:15:02 +0530

    Update zeromat.java

--- zeromat.java
+++ zeromat.java
@@ -1,4 +1,4 @@
-//mat 0
+//Program to check for zeroes in matrix and making the corresponding rows and columns of the zero element as zero
 
 import java.util.*;
 public class zeromat
@@ -8,7 +8,6 @@ public static void main(String []args)
 	int m,n;
 	int k=0;
 	Scanner sc=new Scanner(System.in);
-	ArrayList ll=new ArrayList();
 	m=sc.nextInt();
 	n=sc.nextInt();
 	int [][]ar=new int[m][n];
@@ -17,18 +16,18 @@ public static void main(String []args)
 	for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++){
-		ar[i][j]=sc.nextInt();
-		ar1[i][j]=ar[i][j];
+		ar[i][j]=sc.nextInt();  // main array to get matrix input
+		ar1[i][j]=ar[i][j];  // temp matrix to process
 	}}
 for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++)
-		{if (ar[i][j]==0)
+		{if (ar[i][j]==0)  //checking for zeroes in main matrix
 			{
 				for(int x=0;x<n;x++)
-					ar1[i][x]=0;
+					ar1[i][x]=0;     //converting the corresponding column of temp matrix to zeroes
 				for(int y=0;y<m;y++)
-					ar1[y][j]=0;
+					ar1[y][j]=0;	//converting the corresponding row of temp matrix to zeroes
 			}
 		}
 	}
@@ -37,7 +36,7 @@ for(int i=0;i<m;i++)
 	for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++){
-			System.out.print(ar1[i][j]+" ");
+			System.out.print(ar1[i][j]+" ");    //printing processed temp matrix
 		}
 		System.out.println();
 	}
