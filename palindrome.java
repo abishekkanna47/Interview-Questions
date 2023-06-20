078bd6b8e06dc0a89257f177400062342f7dfc63
AK <93335850+abishekkanna47@users.noreply.github.com>
2023-06-20 21:02:01 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class palindrome
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		LinkedList<String> ll=new LinkedList<String>();
+				LinkedList<String> l=new LinkedList<String>();
+
+		
+		for(int i=0;i<n;i++)
+		{
+		 	ll.add(sc.next());
+			l.add(ll.get(i));
+		}
+	Collections.reverse(ll);
+	for(int i=0;i<ll.size();i++)
+		System.out.println(ll.get(i));
+		if(ll.equals(l))
+		System.out.println("palindrome");
+		else
+				System.out.println("nope");
+
+	}
+}
